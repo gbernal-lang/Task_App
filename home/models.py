c14b9957371e2b1f571fdb05f4f8f73273369445
gbernal-lang <gbernal@cesehsa.com>
2025-11-07 13:59:31 -0600

    Actualizacion

--- home/models.py
+++ home/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class Task(models.Model):
     #Campos del modelo
     titulo = models.CharField(max_length=200)
+    descripcion = models.CharField(verbose_name="Descripcion de tarea",blank=True,null=True)
 
     def __str__(self):
         return self.titulo
