f3c4828390cbafaaf4a8c8eacbfe7cf0feb8c65b
gbernal-lang <gbernal@cesehsa.com>
2025-11-07 15:28:52 -0600

    Se agrega el atributo verbose_name, para que los campos aparezcan en español

--- tasks/models.py
+++ tasks/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 #Modelo con los campos solicitados
 class Task (models.Model):
+ 
 
   #Campos del modelo
-    title = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, verbose_name="Titulo")
+    description = models.CharField(max_length=200, verbose_name="Descripcion")
     status = models.CharField(max_length=200)
-    created_at = models.CharField(max_length=100)
-    updated_at = models.CharField(max_length=100)
-
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Creado por:")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Actualizado por:")
 
     def __str__(self):
-        return "Tareas"
+        return self.title
