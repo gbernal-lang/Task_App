62fd7a81277ea0f5dc35c63efd76e42ce6d4512f
gbernal-lang <gbernal@cesehsa.com>
2025-11-07 15:05:26 -0600

    Se agrego el modelo en admin como prueba

--- home/models.py
+++ home/models.py
@@ -5,10 +5,11 @@ from django.db import models
 
 #se declara un nuevo modelo con nombre Task
 class Task(models.Model):
+    class Meta:
+        verbose_name="Nombre de tareas"
     #Campos del modelo
-    titulo = models.CharField(max_length=200)
+    titulo = models.CharField(max_length=200,verbose_name="Tareas")
     descripcion = models.CharField(verbose_name="Descripcion de tarea",blank=True,null=True)
 
     def __str__(self):
-        return self.titulo
-
+         return f"{self.titulo} {self.descripcion}"
