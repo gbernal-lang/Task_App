822b6c661db57fae85df80c244825d974f203728
gbernal-lang <gbernal@cesehsa.com>
2025-11-10 14:42:01 -0600

    Se agrega la vista para editar tareas, asi como, de sus url

--- tasks/views.py
+++ tasks/views.py
@@ -6,15 +6,15 @@ from django.contrib import messages
 
 class TaskUpdate(UpdateView):
     model = Task
-    template_name = "task_form.html" # Puedes reutilizar la misma del CreateView
-    fields = ['title', 'description', 'status']
+    template_name = "task_form.html" # Template del formulario
+    fields = ['title', 'description', 'status'] #Campos que se pueden editar
     success_url = reverse_lazy('tasks_index')
     
 #Vista basada en funciones, para verificar que se ve en el servidor local
 def index(request):
     return HttpResponse("Pagina de prueba ✅")
 
-
+#Función para mostrar un mensaje de exito.
 def form_valid(self, form):
         messages.success(self.request, "La tarea se edito correctamente :)")
         return super().form_valid(form)
