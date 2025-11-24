4f2d000d6ad9cf930e299c3eb8824c6c18f103aa
gbernal-lang <gbernal@cesehsa.com>
2025-11-24 17:17:47 -0600

    Server

--- app/settings.py
+++ app/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-9x_9fzj--^b2v(!ly5m6%g79#!=mtkt-d&s)=t@uhqp0_q#zjv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
